d8cc515a14e42bf806d550a9448fe2d4da45ffb1
Ethan Friedman <ethanjfriedman@gmail.com>
2016-08-03 12:27:35 -0400

    static server for deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('./build'));
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error('error starting server', err);
+  }  else {
+    console.log('server up and running on port', PORT);
+  }
+});

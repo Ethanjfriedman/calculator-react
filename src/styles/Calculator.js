5f8656e2dd4611936a1767316fee0ad52df0732f
Ethan Friedman <ethanjfriedman@gmail.com>
2016-08-04 12:19:52 -0400

    more boxshadowing

--- src/styles/Calculator.js
+++ src/styles/Calculator.js
@@ -3,5 +3,7 @@ export default {
   margin: '5vh auto',
   border: '2px solid #222',
   boxShadow: '3px 3px 5px #000',
+  WebkitBoxShadow: '3px 3px 5px #000',
+  MozBoxShadow: '3px 3px 5px #000',
   borderRadius: 10
 }

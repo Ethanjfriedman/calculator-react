171a82cf54605bc6f194e41be610155dcf62098b
Ethan Friedman <ethanjfriedman@gmail.com>
2016-08-04 12:17:26 -0400

    boxshadow

--- src/styles/Calculator.js
+++ src/styles/Calculator.js
@@ -2,6 +2,6 @@ export default {
   width: '70vw',
   margin: '5vh auto',
   border: '2px solid #222',
-  borderRadius: 10,
-  boxShadow: '3px 3px 5px #000'
+  boxShadow: '3px 3px 5px #000',
+  borderRadius: 10
 }

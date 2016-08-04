a2972a28cf613fe1bc0ecf4ba2b0f604e8e0633f
Ethan Friedman <ethanjfriedman@gmail.com>
2016-08-04 10:48:49 -0400

    more server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/build'));
-
+app.get('/', (res, res) => {
+  res.send('./build/index.html');
+});
 app.listen(PORT, err => {
   if (err) {
     console.error('error starting server', err);

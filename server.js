d965c6ba9f80a18e07e5c94598473ac06fba59eb
Ethan Friedman <ethanjfriedman@gmail.com>
2016-08-04 12:02:02 -0400

    added static route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/build'));
+app.use('/', express.static(__dirname + '/build'));
 
 app.listen(PORT, err => {
   if (err) {
